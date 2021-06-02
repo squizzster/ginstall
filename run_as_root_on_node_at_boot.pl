4f6f009b760fe4a64e3846b45eae6b8856c83c6e
root <squizzster@gmail.com>
2021-06-02 23:24:22 +0100

    Added run_as_root

---
+++ run_as_root_on_node_at_boot.pl
@@ -0,0 +1,77 @@
+#!/usr/local/bin/perl
+use strict;
+use strictures 2;
+use utf8::all 'GLOBAL';
+use warnings;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
+use diagnostics;
+enable diagnostics;
+
+use Syntax::Keyword::Try;
+use Sys::Hostname; 
+use Socket;
+use Log::Log4perl qw(:easy);
+       ##file     => ">>/var/log/run_as_root_on_node_at_boot.log",
+Log::Log4perl->easy_init( {
+       layout   => '%d{yyyy-MM-dd HH:mm:ss} - %p - %F{1}-%L-%M - %m%n',
+       level    => $INFO,
+       file     => "STDOUT",
+});
+
+my $gbooking_emergency_ip = "";
+my $host_name = get_my_hostname();
+
+INFO "Server [$host_name} has rebooted."; sleep 1;
+
+add_emergency_ip();
+
+
+
+
+exit;
+
+sub get_my_hostname {
+  try {
+    return  hostname();
+  }
+  catch {
+  }
+  return "invalid.unknown.g-booking.com";
+}
+
+
+
+sub add_emergecy_ip {
+  INFO "Adding an emergency firewall IP number";
+  while ( not $gbooking_emergency_ip =~m/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/ ) {
+    $gbooking_emergency_ip = get_emergency_ip();
+  }
+  firewall_cmd ( "--permanent --zone=public --add-rich-rule='  rule family=\"ipv4\"  source address=\"$gbooking_emergency_ip\"   port protocol=\"tcp\" port=\"22\" accept'" );
+  system_ctl   ( "reload", "firewalld" );
+  INFO "I've added emergency IP [$gbooking_emergency_ip] into the firewall table and reloaded the firewall.";
+}
+
+
+sub get_emergency_ip {
+  try {
+    return inet_ntoa(inet_aton('emergency.g-booking.com'));
+  }
+  catch {
+    WARN "Emergency address error. $@";
+    sleep 2;
+    return "";
+  }
+}
+
+
+sub firewall_cmd {
+  return `/usr/bin/firewall-cmd $_[0] 2>&1` if $_[0] or return;
+}
+
+
+sub system_ctl {
+  return `/usr/bin/systemctl $_[0] $_[1]` if ( $_[0] and $_[1] ) or return; 
+}
+
+
--- runme.sh
+++ runme.sh
@@ -29,12 +29,28 @@ echo "g-Booking server installation started
 [ -d "/gbooking" ] && echo "g-booking directory exists...I am aborting so you can have a rethink." || echo "Thunderbirds are GO!"
 [ -d "/gbooking" ] && exit
 
+dnf -y install wget
+cd /root
+rm -rf cpan* perl* ccrypt*
+
+echo "Fetching scripts and data files..."
+wget -c https://github.com/squizzster/ginstall/raw/master/cpan.tar 
+wget -c https://g-booking.fra1.digitaloceanspaces.com/public/perl-5.34.0.tar.gz
+wget -c https://github.com/squizzster/ginstall/raw/master/ccrypt-1.11.tar.gz
+wget -c https://github.com/squizzster/ginstall/raw/master/install_cpan.pl 
+wget -c https://github.com/squizzster/ginstall/raw/master/encode_decode
+
+echo "Installing encode_decode"
+mv /root/encode_decode /usr/local/bin
+chmod 555 /usr/local/bin/encode_decode
+
+echo "Updating system"
 dnf -y update 
 dnf -y upgrade
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf -y config-manager --set-enabled powertools
 curl -L https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
-dnf -y install firewalld gcc gcc-c++ make openssl-devel git libdb-devel openssl-devel rclone libaio libsepol lsof boost-program-options wget
+dnf -y install firewalld gcc gcc-c++ make openssl-devel git libdb-devel openssl-devel rclone libaio libsepol lsof boost-program-options
 dnf -y install MariaDB-server MariaDB-client mod_ssl redis mysql-devel memcached.x86_64 libmemcached.x86_64 libmemcached-libs.x86_64
 
 systemctl enable mariadb
@@ -47,11 +63,6 @@ echo "Adding zones";
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
 systemctl enable firewalld 
 
-cd /root
-rm -rf cpan* perl* ccrypt*
-wget -c https://github.com/squizzster/ginstall/raw/master/cpan.tar 
-wget -c https://g-booking.fra1.digitaloceanspaces.com/public/perl-5.34.0.tar.gz
-wget -c https://github.com/squizzster/ginstall/raw/master/ccrypt-1.11.tar.gz
 
 gzip -fd ccrypt-1.11.tar.gz
 tar -xf ccrypt-1.11.tar
