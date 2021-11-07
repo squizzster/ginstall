9ce73fee9afdc6f04215b78754c30bcbd88632b3
root <squizzster@gmail.com>
2021-11-07 03:50:47 +0000

    Updateed with some junk to see if gitlab notices

--- install_me.sh
+++ install_me.sh
@@ -9,7 +9,7 @@ dnf -y update
 
 dnf -y install firewalld gcc gcc-c++ make openssl-devel git libdb-devel openssl-devel rclone libaio libsepol lsof boost-program-options MariaDB-server MariaDB-client mod_ssl redis mysql-devel memcached.x86_64 libmemcached.x86_64 libmemcached-libs.x86_64 systemd-devel systemd-libs cpan traceroute telnet sysbench libpng-devel zlib-devel  libgcrypt libgcrypt-devel compat-libpthread-nonshared bzip2 google-authenticator qrencode-libs bind-utils ncdu nodejs libsecret-devel gnupg1.x86_64 scl-utils gcc-toolset-9 git cmake3 zlib-devel boost-devel boost boost-devel glpk glpk-devel nload wget chrony firewalld tar bind-utils.x86_64 curl
 
-##   boost-build    (removed)
+##   boost-build    (removed but needed to build osrm)
 ##   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
 #wget ftp://ftp.pbone.net/mirror/ftp.centos.org/8.4.2105/PowerTools/x86_64/os/Packages/asio-devel-1.10.8-7.module_el8.3.0+757+d382997d.x86_64.rpm
