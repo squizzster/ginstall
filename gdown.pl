9b0645078abc48127786bbf7e5ae230c57e071d8
root <squizzster@gmail.com>
2021-11-09 07:06:19 +0000

    OK

--- gdown.pl
+++ gdown.pl
@@ -89,7 +89,7 @@ sub execute_command() {
         }
     }
 
-    $COMMAND="wget $CONTINUE --progress=dot:giga --no-check-certificate --load-cookie $TEMP --save-cookie $TEMP \"$URL\"";
+    $COMMAND="wget $CONTINUE --no-check-certificate --load-cookie $TEMP --save-cookie $TEMP \"$URL\"";
     $COMMAND.=" -O \"$OUTPUT_FILENAME\"";
     my $OUTPUT = system( $COMMAND );
     if ( $OUTPUT == 2 ) { # do a clean exit with Ctrl+C
