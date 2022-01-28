4581a055ccc868c2732cd3a63909cce58de5b73a
root <squizzster@gmail.com>
2022-01-28 01:11:37 +0000

    OK

--- install_me.sh
+++ install_me.sh
@@ -1,32 +1,38 @@
 #!/bin/sh
 
-### NEW CPAN:
-#    GIS::Distance::Fast
-#    GIS::Distance
-#    cpanm GIS::Distance::Fast  GIS::Distance
-
 echo "One moment...."
 sleep 1
 
-
-### stuff here can fail....
 for (( c=1; c<=500; c++ ))
 do  
    echo "
 "
 done
 
+cd /root
+BASEDIR=$(pwd)
+
+if test "$BASEDIR" != '/root'; then
+  echo "I am NOT in /root which is where I want to be.  Check I am running as root";
+  exit;
+fi
+
+echo 'CiAgICAgICAgICAgICAgICAgICAgLiAgICwtIFRvIHRoZSBNb29uIE1hcmsgIQogICAgICAgICAgICAgICAgICAgLicuCiAgICAgICAgICAgICAgICAgICB8b3wKICAgICAgICAgICAgICAgICAgLidvJy4KICAgICAgICAgICAgICAgICAgfC4tLnwKICAgICAgICAgICAgICAgICAgJyAgICcKICAgICAgICAgICAgICAgICAgICggKQogICAgICAgICAgICAgICAgICAgICkKICAgICAgICAgICAgICAgICAgICggKQoKICAgICAgICAgICAgICAgX19fXwogICAgICAgICAgLi0nIiJwIDhvIiJgLS4KICAgICAgIC4tJzg4ODhQJ1kuYFlbICcgYC0uCiAgICAgLCddODg4ODhiLko4b29fICAgICAgJ2AuCiAgICwnICw4ODg4ODg4ODg4OFsiICAgICAgICBZYC4KICAvICAgODg4ODg4ODg4OFAgICAgICAgICAgICBZOFwKIC8gICAgWTg4ODg4ODhQJyAgICAgICAgICAgICBdODhcCjogICAgIGBZODgnICAgUCAgICAgICAgICAgICAgYDg4ODoKOiAgICAgICBZOC5vUCAnLSA+ICAgICAgICAgICAgWTg4Ogp8ICAgICAgICAgIGBZYiAgX18gICAgICAgICAgICAgYCd8CjogICAgICAgICAgICBgJ2Q4ODg4Ym8uICAgICAgICAgIDoKOiAgICAgICAgICAgICBkODg4ODg4ODhvb28uICAgICAgOwogXCAgICAgICAgICAgIFk4ODg4ODg4ODg4OFAgICAgIC8KICBcICAgICAgICAgICAgYFk4ODg4ODg4OFAgICAgIC8KICAgYC4gICAgICAgICAgICBkODg4ODhQJyAgICAsJwogICAgIGAuICAgICAgICAgIDg4OFBQJyAgICAsJwogICAgICAgYC0uICAgICAgZDhQJyAgICAsLScgICAtZy1Cb29raW5nLQogICAgICAgICAgYC0uLCxfJ19fLCwuLScKCgogICAgICAgVEhVTkRFQklSRFMgQVJFIEdPISEK' | base64 -d
+ 
 [ -f /etc/redhat-release ] && echo "" || exit 
 [ -d "/gbooking" ] && echo "g-booking directory exists...I am aborting so you can have a rethink." || echo "Thunderbirds are GO!"
 [ -d "/gbooking" ] && exit
 sleep 1
-echo "Thunderbird 5,  starting lift off...
+echo "
+
+Thunderbird 5,  starting lift off...
 ";
-sleep 1
+sleep 2
 
 echo "Installing EPL release..."
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm >/root/install.log 2>/root/install.err
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm >/root/install.log 2>/root/install.err
+
 echo "Installing config manager..."
 dnf -y config-manager --set-enabled powertools >>/root/install.log 2>>/root/install.err
 dnf -y config-manager --set-enabled powertools >>/root/install.log 2>>/root/install.err
@@ -41,7 +47,6 @@ export PATH=/usr/pgsql-12/bin:$PATH
 
 #echo "Installing PostGres GIS"
 #dnf -y install postgresql12-server postgresql12-contrib  postgresql12-devel postgis30_12 >>/root/install.log 2>>/root/install.err
-
 
 echo "Installing Boost Build"
 dnf -y install boost-build >>/root/install.log 2>>/root/install.err
@@ -63,16 +68,13 @@ catch() {
 
 ## don't think we need this and it takes up resource
 echo "Performing an upgrade of everything..."
-sleep 1
 dnf -y upgrade >>/root/install.log 2>>/root/install.err
-sleep 1
+
 ### We install MariaDB from our own RPM now....
 #mariadb mariadb-common mariadb-devel mariadb-server mariadb-server-galera mariadb-server-utils 
 
-echo "Installing all required DNF modules..."
-sleep 2
-dnf -y install net-tools firewalld gcc gcc-c++ make openssl-devel git libdb-devel openssl-devel rclone libaio libsepol lsof boost-program-options mod_ssl redis  memcached.x86_64 libmemcached.x86_64 libmemcached-libs.x86_64 systemd-devel systemd-libs cpan traceroute telnet sysbench libpng-devel zlib-devel  libgcrypt libgcrypt-devel compat-libpthread-nonshared bzip2 google-authenticator qrencode-libs bind-utils ncdu nodejs libsecret-devel gnupg1.x86_64 scl-utils gcc-toolset-9 git cmake3 zlib-devel boost-devel boost boost-devel glpk glpk-devel nload wget chrony firewalld tar bind-utils.x86_64 policycoreutils-python-utils curl >>/root/install.log 2>>/root/install.err
-
+echo "Installing required modules..."
+dnf -y install net-tools firewalld gcc gcc-c++ make openssl-devel git libdb-devel openssl-devel rclone libaio libsepol lsof boost-program-options mod_ssl redis  memcached.x86_64 libmemcached.x86_64 libmemcached-libs.x86_64 systemd-devel systemd-libs cpan traceroute telnet sysbench libpng-devel zlib-devel  libgcrypt libgcrypt-devel compat-libpthread-nonshared bzip2 google-authenticator qrencode-libs bind-utils ncdu nodejs libsecret-devel gnupg1.x86_64 scl-utils gcc-toolset-9 git cmake3 zlib-devel boost-devel boost boost-devel glpk glpk-devel nload wget chrony firewalld tar bind-utils.x86_64 policycoreutils-python-utils curl kpatch >>/root/install.log 2>>/root/install.err
 
 #wget ftp://ftp.pbone.net/mirror/ftp.centos.org/8.4.2105/PowerTools/x86_64/os/Packages/asio-devel-1.10.8-7.module_el8.3.0+757+d382997d.x86_64.rpm
 #dnf -y install asio-devel-1.10.8-7.module_el8.3.0+757+d382997d.x86_64.rpm
@@ -82,6 +84,7 @@ dnf -y install net-tools firewalld gcc gcc-c++ make openssl-devel git libdb-deve
 ## all authentication is done locally and we have our own centralised security system
 authselect select minimal with-silent-lastlog --force >>/root/install.log 2>>/root/install.err 
 
+echo "Configuring firewall..."
 EIP=`dig emergency.g-booking.com +short`
 
 echo -n '<?xml version="1.0" encoding="utf-8"?>
@@ -102,26 +105,20 @@ echo -n '"/>
 
 echo "Enabling the firewall...."
 systemctl enable firewalld >>/root/install.log 2>>/root/install.err
-systemctl start firewalld >>/root/install.log 2>>/root/install.err
 systemctl restart firewalld >>/root/install.log 2>>/root/install.err
 
-cd /root
-BASEDIR=$(pwd)
-
-if test "$BASEDIR" != '/root'; then
-  echo "I am NOT in /root which is where I want to be.  Check I am running as root";
-  exit;
-fi
-
-
+echo "Patching the kernel...."
+dnf -y kpatch auto      >>/root/install.log 2>>/root/install.err
+systemctl enable kpatch >>/root/install.log 2>>/root/install.err
+systemctl start kpatch  >>/root/install.log 2>>/root/install.err
 
 echo "Setting timezone to UTC"
 timedatectl set-timezone UTC >>/root/install.log 2>>/root/install.err
 
-echo "Enabling chronyd"
+echo "Enabling time keeping"
 systemctl enable chronyd >>/root/install.log 2>>/root/install.err
 
-echo "Staring chronyd"
+echo "Starting chronyd"
 systemctl start chronyd >>/root/install.log 2>>/root/install.err
 cd /root
 
@@ -130,39 +127,39 @@ cd /root
 #curl -s -L https://github.com/squizzster/ginstall/raw/master/gdown.pl >/usr/local/bin/gdown.pl 2>>/root/install.err
 #chmod 555 /usr/local/bin/gdown.pl 
 
-echo "Download MariaDB"
-rm -f mariadb-10.*.tar
-wget --read-timeout=30 --tries=10  --quiet https://agitated-ramanujan-4baf2c.netlify.app/mariadb-10.6.5-rhel-8-x86_64-rpms.tar 2>>/root/install.err
+#echo "Download MariaDB 10.6.5 from our own webserver."
+#rm -f mariadb-10.*.tar
+#wget --read-timeout=30 --tries=10  --quiet https://agitated-ramanujan-4baf2c.netlify.app/mariadb-10.6.5-rhel-8-x86_64-rpms.tar 2>>/root/install.err
 
 #####wget https://storage.googleapis.com/g-booking-install/mariadb-10.6.5-rhel-8-x86_64-rpms.tar  >>/root/install.log 2>>/root/install.err
 
-echo "Extracting MariaDB...."
-cd /opt
-tar -xf /root/mariadb-10.6.5-rhel-8-x86_64-rpms.tar >>/root/install.log 2>>/root/install.err
-/opt/mariadb-10.6.5-rhel-8-x86_64-rpms/setup_repository >>/root/install.log 2>>/root/install.err
-rm -f /root/mariadb-10.6.5-rhel-8-x86_64-rpms.tar >>/root/install.log 2>>/root/install.err
-echo "Installing MariaDB"
-dnf -y install MariaDB-server MariaDB-common MariaDB-s3-engine MariaDB-devel MariaDB-backup MariaDB >>/root/install.log 2>>/root/install.err
+#echo "Extracting MariaDB...."
+#cd /opt
+#tar -xf /root/mariadb-10.6.5-rhel-8-x86_64-rpms.tar >>/root/install.log 2>>/root/install.err
+#/opt/mariadb-10.6.5-rhel-8-x86_64-rpms/setup_repository >>/root/install.log 2>>/root/install.err
+#rm -f /root/mariadb-10.6.5-rhel-8-x86_64-rpms.tar >>/root/install.log 2>>/root/install.err
+#echo "Installing MariaDB"
+#dnf -y install MariaDB-server MariaDB-common MariaDB-s3-engine MariaDB-devel MariaDB-backup MariaDB >>/root/install.log 2>>/root/install.err
 
 ### Need to check error messsage and then actually decide what to do otherwise just x 2 chances of exiting the script but we dont have one of those
 
-echo "Updating system"
-systemctl enable mariadb >>/root/install.log 2>>/root/install.err
-systemctl start mariadb >>/root/install.log 2>>/root/install.err
-echo "Adding mysql_upgrade";
-nohup mysql_upgrade >>/root/install.log 2>>/root/install.err &
+#echo "Updating system"
+#systemctl enable mariadb >>/root/install.log 2>>/root/install.err
+#systemctl start mariadb >>/root/install.log 2>>/root/install.err
+#echo "Adding mysql_upgrade";
+#nohup mysql_upgrade >>/root/install.log 2>>/root/install.err &
 
-sleep 20;
+#sleep 20;
 
-echo "Securing mysql"
-echo 'RFJPUCBEQVRBQkFTRSBJRiBFWElTVFMgdGVzdDsKREVMRVRFIEZST00gbXlzcWwudXNlciBXSEVSRSBVc2VyPSdyb290JyBBTkQgSG9zdCBOT1QgSU4gKCdsb2NhbGhvc3QnLCAnMTI3LjAuMC4xJywgJzo6MScpOwpERUxFVEUgRlJPTSBteXNxbC51c2VyIFdIRVJFIFVzZXI9Jyc7CkRFTEVURSBGUk9NIG15c3FsLmRiIFdIRVJFIERiPSd0ZXN0JyBPUiBEYj0ndGVzdFxfJSc7CkZMVVNIIFBSSVZJTEVHRVM7Cgo=' | base64 -d | mysql >>/root/install.log 2>>/root/install.err
+#echo "Securing mysql"
+#echo 'RFJPUCBEQVRBQkFTRSBJRiBFWElTVFMgdGVzdDsKREVMRVRFIEZST00gbXlzcWwudXNlciBXSEVSRSBVc2VyPSdyb290JyBBTkQgSG9zdCBOT1QgSU4gKCdsb2NhbGhvc3QnLCAnMTI3LjAuMC4xJywgJzo6MScpOwpERUxFVEUgRlJPTSBteXNxbC51c2VyIFdIRVJFIFVzZXI9Jyc7CkRFTEVURSBGUk9NIG15c3FsLmRiIFdIRVJFIERiPSd0ZXN0JyBPUiBEYj0ndGVzdFxfJSc7CkZMVVNIIFBSSVZJTEVHRVM7Cgo=' | base64 -d | mysql >>/root/install.log 2>>/root/install.err
 
-echo "Adding zones";
-mysql_tzinfo_to_sql /usr/share/zoneinfo 2>>/root/install.err | mysql -u root mysql >>/root/install.log 2>>/root/install.err
-systemctl enable firewalld  >>/root/install.log 2>>/root/install.err
+#echo "Adding zones";
+#mysql_tzinfo_to_sql /usr/share/zoneinfo 2>>/root/install.err | mysql -u root mysql >>/root/install.log 2>>/root/install.err
+
 
 cd /root
-echo "Final tasks....."
+echo "Configuring SSH..."
 ### OK, some final task..
 echo '##### gbooking v1.0 ######
 HostKey /etc/ssh/ssh_host_ed25519_key
@@ -203,6 +200,7 @@ PermitTunnel no
 mkdir -p /root/.ssh
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMF5PmJ+ma3VLnPWsGctt+MSxd8l1Cfzz27E/Em2xSe2 root@g-booking.com' >/root/.ssh/authorized_keys
 
+echo "Installing node_checker..."
 curl -s -L https://github.com/squizzster/ginstall/raw/master/node_checker >/usr/local/bin/node_checker 2>>/root/install.err
 chmod 100 /usr/local/bin/node_checker
 echo '[Unit]
@@ -246,14 +244,16 @@ systemctl enable node_checker >>/root/install.log 2>>/root/install.err
 
 cd /
 
+encho "Installing perl...";
 curl -s -L https://github.com/squizzster/ginstall/raw/master/install_perl-5.34.0.tar.gz | tar -zx >>/root/install.log 2>>/root/install.err
+encho "Installing CPAN...";
 curl -s -L https://github.com/squizzster/ginstall/raw/master/cpan_only_modules_03_11_21.tar.gz | tar -zx >>/root/install.log 2>>/root/install.err
 curl -s -L https://github.com/squizzster/ginstall/raw/master/cpan_only_modules_05_11_21.tar.gz | tar -zx >>/root/install.log 2>>/root/install.err
 curl -s -L https://github.com/squizzster/ginstall/raw/master/cpan_only_modules_07_11_21.tar.gz | tar -zx >>/root/install.log 2>>/root/install.err
 
 
-systemctl stop mariadb >>/root/install.log 2>>/root/install.err
-systemctl disable mariadb >>/root/install.log 2>>/root/install.err
+#systemctl stop mariadb >>/root/install.log 2>>/root/install.err
+#systemctl disable mariadb >>/root/install.log 2>>/root/install.err
 
 echo "Removing unused daemons like sssd-client and polkit"
 dnf -y remove polkit sssd-client  sssd-common  sssd-kcm   sssd-nfs-idmap  >>/root/install.log 2>>/root/install.err
